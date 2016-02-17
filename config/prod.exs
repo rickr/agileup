9fefc998a56a63dcf74d615d77238e98eb323a30
RickR <rrein@akamai.com>
2016-02-16 23:15:42 -0500

    update prod psql

--- config/prod.exs
+++ config/prod.exs
@@ -19,6 +19,9 @@ config :agileup, Agileup.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :agileup, Agileup.Repo,
+ database: "agileup"
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
