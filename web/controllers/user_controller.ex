0da31e15c16fc4076826511d1028648216937200
RickR <rrein@akamai.com>
2016-02-16 22:29:17 -0500

    allow user to update settings

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -3,8 +3,9 @@ defmodule Agileup.UserController do
   use Agileup.Web, :controller
   use Guardian.Phoenix.Controller
 
-  plug Guardian.Plug.EnsureAuthenticated, handler: Agileup.Access
+  alias Agileup.User
 
+  plug Guardian.Plug.EnsureAuthenticated, handler: Agileup.Access
 
   # GET /settings
   def edit(conn, _params, user, _claims) do
@@ -12,7 +13,21 @@ defmodule Agileup.UserController do
   end
 
   # POST /settings
-  def update(conn, _params, user, _claims) do
-    render conn, "edit.html"
+  def update(conn, %{"settings" => settings}, user, _claims) do
+    Logger.debug "Updating user id #{user.id} with #{inspect settings}"
+
+    changeset = User.changeset(user, settings)
+    Logger.debug(inspect changeset)
+    if changeset.valid? do
+      Repo.update(changeset)
+
+      conn
+      |> put_flash(:success, "Your settings have been saved")
+      |> redirect to: page_path(conn, :index)
+    else
+      conn
+      |> put_flash(:error, "There was a problem saving your settings.")
+      |> render("edit.html", user: user, changeset: changeset)
+    end
   end
 end
